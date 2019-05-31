fc7ca135f7fbdc5cc5090ba814decf9c58411b58
Andrea N <andreain3d@gmail.com>
2019-05-31 16:30:12 -0400

    added click functionality to update values, winning/losing conditions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,8 +2,13 @@ var wins = 0;
 var losses = 0;
 
 var game = function() {
-  //Sets target value and write to page
   var targetNumber = 0;
+  var totalValue = 0;
+  $(".snackTotal").text(totalValue);
+  $(".wins").text(wins);
+  $(".losses").text(losses);
+
+  //Sets target value and write to page
   do {
     targetNumber = Math.floor(Math.random() * 121);
   } while (targetNumber < 19);
@@ -42,6 +47,68 @@ var game = function() {
     appleValue === iceCreamValue
   );
   console.log(appleValue);
+
+  //Defining functions to execute when win/loss conditions are met
+  var youWin = function() {
+    setTimeout(function() {
+      alert("you win!");
+    }, 500);
+    wins += 1;
+    $(".wins").text(wins);
+  };
+
+  var youLose = function() {
+    setTimeout(function() {
+      alert("you lose!");
+    }, 100);
+    losses += 1;
+    $(".losses").text(losses);
+  };
+
+  //When user clicks on food items, their value is added to the total and written to
+  //the page.  Then, winning and losing conditions are checked, and if met will
+  //execute win/loss function
+  $("#hotdog").on("click", function() {
+    totalValue += hotDogValue;
+    $(".snackTotal").text(totalValue);
+    if (totalValue == targetNumber) {
+      youWin();
+    } else if (totalValue > targetNumber) {
+      youLose();
+    }
+  });
+
+  $("#cookie").on("click", function() {
+    totalValue += cookieValue;
+    $(".snackTotal").text(totalValue);
+    if (totalValue == targetNumber) {
+      youWin();
+    } else if (totalValue > targetNumber) {
+      youLose();
+    }
+  });
+
+  $("#iceCream").on("click", function() {
+    totalValue += iceCreamValue;
+    $(".snackTotal").text(totalValue);
+    if (totalValue == targetNumber) {
+      youWin();
+    } else if (totalValue > targetNumber) {
+      youLose();
+    }
+  });
+
+  $("#apple").on("click", function() {
+    totalValue += appleValue;
+    $(".snackTotal").text(totalValue);
+    if (totalValue == targetNumber) {
+      youWin();
+    } else if (totalValue > targetNumber) {
+      youLose();
+    }
+  });
+
+  //Restart functionality
 };
 
 game();
